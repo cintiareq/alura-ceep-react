109dcfd86cc15f3eab102a4b14339c3b43fdc08f
cintiareq <cintia.rl94@gmail.com>
2021-02-11 17:06:42 -0300

    updates

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './style.css';
+
+class Header extends Component {
+  render() {
+    return (
+
+      <section className="header-wrap">
+        <div className="header">
+          <h1 className="header_text">ceep</h1>
+
+        </div>
+      </section>
+    );
+  }
+
+}
+
+export default Header;
