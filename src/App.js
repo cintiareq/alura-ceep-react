807eef73f4c611966ed6513010436e39a7ab50b4
Cintia <cintia.rl94@gmail.com>
2021-02-12 10:21:17 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import './assets/App.css'
 import './assets/index.css'
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      notas: []
+    }
+  }
+
+  criarNota(titulo, texto) {
+  const novaNota = {titulo, texto};
+  const novoArrayNotas = [...this.state.notas, novaNota]
+  const novoEstado = {
+    notas:novoArrayNotas
+  }
+  this.setState(novoEstado)
+}
   render() {
     return (
       <section className="conteudo">
 
-        <Header />        <FormularioCadastro />
-        <ListaDeNotas />
+        <Header />        
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
 
       </section>
     );
